02bbf2ec275f975ba27817494b2a5b8ec289ee30
devdiagon <fredericktimo@gmail.com>
2025-09-19 18:19:51 -0500

    initial display for sites

---
+++ composeApp/src/commonMain/kotlin/io/devdiagon/multisites/Site.kt
@@ -0,0 +1,17 @@
+package io.devdiagon.multisites
+
+data class Site(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val description: String
+)
+
+val sites = (1..100).map {
+    Site(
+        id = it,
+        name = "Site $it",
+        image = "https://picsum.photos/200/300?id=$it",
+        description = "Description not that long to catch the attention of the user $it"
+    )
+}
