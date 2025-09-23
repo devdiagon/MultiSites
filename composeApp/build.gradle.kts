b51cad9089f027dbf74ef2bde190082f4323ba3a
devdiagon <fredericktimo@gmail.com>
2025-09-23 09:50:09 -0500

    updated Content Negotiation dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -44,7 +44,7 @@ kotlin {
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor)
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.contentegotiation)
+            implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.androidx.navigation.compose)
         }
