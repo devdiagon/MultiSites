6f89e185f37733fc877e70e620b67d646826a35e
devdiagon <fredericktimo@gmail.com>
2025-09-28 16:27:05 -0500

    style: improved detail screen style

--- composeApp/src/commonMain/kotlin/io/devdiagon/multisites/ui/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/io/devdiagon/multisites/ui/screens/detail/DetailScreen.kt
@@ -1,5 +1,6 @@
 package io.devdiagon.multisites.ui.screens.detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,61 +16,126 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import io.devdiagon.multisites.data.models.Site
 import io.devdiagon.multisites.ui.common.LoadingIndicator
 import io.devdiagon.multisites.ui.screens.Screen
 import multisites.composeapp.generated.resources.Res
 import multisites.composeapp.generated.resources.back
+import multisites.composeapp.generated.resources.site_kinds
+import multisites.composeapp.generated.resources.site_location
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(vm: DetailViewModel, onBack: () -> Unit) {
     val state = vm.state
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Screen {
         Scaffold(
             topBar = {
-                TopAppBar(
-                    title = { Text(state.site?.name ?: "") },
-                    navigationIcon = {
-                        IconButton(onClick = onBack) {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                                contentDescription = stringResource(Res.string.back)
-                            )
-                        }
-                    }
+                DetailTopBar(
+                    title = state.site?.name ?: "",
+                    onBack = onBack,
+                    scrollBehavior = scrollBehavior
                 )
             }
         ) { padding ->
-            LoadingIndicator(enabled = state.loading)
+            LoadingIndicator(
+                enabled = state.loading,
+                Modifier.padding(padding)
+            )
 
             state.site?.let { site ->
-                Column(
-                    modifier = Modifier
-                        .padding(padding)
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    AsyncImage(
-                        model = site.image,
-                        contentDescription = site.name,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(16f / 9f)
-                    )
-                    Text(
-                        text = site.description,
-                        modifier = Modifier.padding(16.dp),
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-                }
+                SiteDetail(
+                    site = site,
+                    modifier = Modifier.padding(padding)
+                )
             }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DetailTopBar(
+    title: String,
+    onBack: () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = stringResource(Res.string.back)
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+@Composable
+private fun SiteDetail(
+    site: Site,
+    modifier: Modifier = Modifier
+) {
+    val kindsFormated: String = site.kinds.replace("_", " ")
+    //val kindsFormated: List<String> = site.kinds.split(",").map { it.replace("_", " ") }
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        AsyncImage(
+            model = site.image,
+            contentDescription = site.name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(16f / 9f)
+        )
+        Text(
+            text = site.description,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = buildAnnotatedString {
+                property(name = stringResource(Res.string.site_location), value = site.city + ", " + site.country)
+                property(name = stringResource(Res.string.site_kinds), value = kindsFormated, end = true)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                .padding(16.dp)
+        )
+    }
+}
+
+private fun AnnotatedString.Builder.property(name: String, value: String, end: Boolean = false) {
+    withStyle(ParagraphStyle(lineHeight = 18.sp)) {
+        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+            append("$name: ")
+        }
+        append(value)
+        if(!end) {
+            append("\n")
         }
     }
 }
