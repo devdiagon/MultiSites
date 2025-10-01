38679e714f9daa7b1bb42269fc9ba135fe51b820
devdiagon <fredericktimo@gmail.com>
2025-10-01 10:44:10 -0500

    refactor: parameters are specified in the API URL

--- composeApp/src/commonMain/kotlin/io/devdiagon/multisites/data/SitesService.kt
+++ composeApp/src/commonMain/kotlin/io/devdiagon/multisites/data/SitesService.kt
@@ -5,11 +5,21 @@ import io.devdiagon.multisites.data.payload.RawPlacesReq
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 class SitesService(private val client : HttpClient) {
     suspend fun fetchRawSitesIds(): RawPlacesReq {
         return client
-            .get("/0.1/en/places/radius?radius=10000&lon=-78.4678&lat=-0.1807&limit=15&kinds=museums,historic,foods,architecture,natural&rate=3")
+            .get("/0.1/en/places/radius") {
+                // HARDCODED parameters
+                parameter("radius","10000")
+                parameter("lon","-78.4678")
+                parameter("lat","-0.1807")
+                parameter("limit","15")
+                parameter("kinds","museums,historic,foods,architecture,natural")
+                parameter("rate", "3")
+            }
+            //.get("/0.1/en/places/radius?radius=10000&lon=-78.4678&lat=-0.1807&limit=15&kinds=museums,historic,foods,architecture,natural&rate=3")
             .body<RawPlacesReq>()  //Serialize the result
     }
 
