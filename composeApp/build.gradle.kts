b48a638e0a8d9358a0f99b4bf9fae355b23b3d82
devdiagon <fredericktimo@gmail.com>
2025-10-01 12:41:12 -0500

    libs: added play services location dependency for Android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,6 +35,7 @@ kotlin {
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
             implementation(libs.koin.android)
+            implementation(libs.play.services.location)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
